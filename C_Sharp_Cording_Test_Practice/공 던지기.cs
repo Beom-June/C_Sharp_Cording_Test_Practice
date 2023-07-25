ffa0f9db3d55723bf7a30cd8a0084b00b7f50482
= <zxo9988@naver.com>
2023-07-25 12:19:04 +0900

    2문제 push

---
+++ C_Sharp_Cording_Test_Practice/공 던지기.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 머쓱이는 친구들과 동그랗게 서서 공 던지기 게임을 하고 있습니다. 
+ * 공은 1번부터 던지며 오른쪽으로 한 명을 건너뛰고 
+ * 그 다음 사람에게만 던질 수 있습니다. 친구들의 번호가 들어있는 
+ * 정수 배열 numbers와 정수 K가 주어질 때, 
+ * k번째로 공을 던지는 사람의 번호는 무엇인지 return 하도록 
+ * solution 함수를 완성해보세요.
+ */
+namespace C_Sharp_Cording_Test_Practice
+{
+    /*
+    public class Solution
+    {
+        public int solution(int[] numbers, int k)
+        {
+            int answer = numbers[(k * 2 - 2) % numbers.Length];
+            return answer;
+        }
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        int ball = 1;
+        
+        for(int i = 0; i < k - 1; i++)
+        {
+            if(ball + 2 > numbers.Length)
+            {
+                ball += 2;
+                ball -= numbers.Length;
+            }
+            else
+            {
+                ball += 2;
+            }
+        }
+        answer = ball;
+        return answer;
+    }
+    }
+    class 공_던지기
+    {
+        static void Main(string[] args)
+        {
+            Solution _solution = new Solution();
+
+            int[] _numbers1 = { 1, 2, 3, 4 };
+            int[] _numbers2 = { 1, 2, 3, 4, 5, 6 };
+            int[] _numbers3 = { 1, 2, 3 };
+
+            int _k1 = 2;
+            int _k2 = 5;
+            int _k3 = 3;
+
+            Console.WriteLine(_solution.solution(_numbers1, _k1));
+            Console.WriteLine(_solution.solution(_numbers2, _k2));
+            Console.WriteLine(_solution.solution(_numbers3, _k3));
+        }
+    }
+    */
+}
---
+++ C_Sharp_Cording_Test_Practice/배열 회전 시키기.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 정수가 담긴 배열 numbers와 문자열 direction가 매개변수로 주어집니다. 
+ * 배열 numbers의 원소를 direction방향으로 한 칸씩 회전시킨 
+ * 배열을 return하도록 solution 함수를 완성해주세요.
+ */
+namespace C_Sharp_Cording_Test_Practice
+{
+    /*
+    public class Solution
+    {
+        public int[] solution(int[] numbers, string direction)
+        {
+            int[] answer = new int[numbers.Length];
+
+            if (direction == "right")
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    int _Idx = (i + 1) % numbers.Length;
+                    answer[_Idx] = numbers[i];
+                }
+            }
+            else if (direction == "left")
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    int _Idx = (i - 1 + numbers.Length) % numbers.Length;
+                    answer[_Idx] = numbers[i];
+                }
+            }
+            return answer;
+        }
+    }
+    class 배열_회전_시키기
+    {
+        static void Main(string[] args)
+        {
+            Solution _solution = new Solution();
+
+            int[] _numbers1 = { 1, 2, 3 };
+            int[] _numbers2 = { 4, 455, 6, 4, -1, 45, 6 };
+
+            string _direction1 = "right";
+            string _direction2 = "left";
+
+            int[] result1 = _solution.solution(_numbers1, _direction1);
+            Console.WriteLine(string.Join(", ", result1)); // 출력: 3, 1, 2
+
+            int[] result2 = _solution.solution(_numbers2, _direction2);
+            Console.WriteLine(string.Join(", ", result2)); // 출력: 455, 6, 4, -1, 45, 6, 4
+        }
+    }
+    */
+}
