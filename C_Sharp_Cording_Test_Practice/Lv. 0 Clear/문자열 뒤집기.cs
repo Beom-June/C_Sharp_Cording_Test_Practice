dc475168c76e3c42d110a63603264a05f4612077
= <zxo9988@naver.com>
2023-08-28 11:51:01 +0900

    문제 정리

--- C_Sharp_Cording_Test_Practice/Lv. 0 Clear/문자열 뒤집기.cs
+++ C_Sharp_Cording_Test_Practice/Lv. 0 Clear/문자열 뒤집기.cs
@@ -4,20 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/* 문자열 my_string이 매개변수로 주어집니다. 
- * my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+/* 문자열 my_string과 정수 s, e가 매개변수로 주어질 때, 
+ * my_string에서 인덱스 s부터 인덱스 e까지를 뒤집은 
+ * 문자열을 return 하는 solution 함수를 작성해 주세요.
  */
 namespace C_Sharp_Cording_Test_Practice
 {
     /*
     public class Solution
     {
-        public string solution(string my_string)
+        public string solution(string my_string, int s, int e)
         {
-            char[] _array = my_string.ToCharArray();
-            Array.Reverse(_array);
+            char[] chars = my_string.ToCharArray();
 
-            return new string(_array);
+            while (s < e)
+            {
+                char temp = chars[s];
+                chars[s] = chars[e];
+                chars[e] = temp;
+
+                s++;
+                e--;
+            }
+
+            return new string(chars);
         }
     }
     class 문자열_뒤집기
@@ -26,12 +36,24 @@ namespace C_Sharp_Cording_Test_Practice
         {
             Solution _solution = new Solution();
 
-            string my_string = "jaron";
-            string _result = _solution.solution(my_string);
+            string _my_string1 = "Progra21Sremm3";
+            string _my_string2 = "Stanley1yelnatS";
 
-            Console.WriteLine(_result);
+            int _s1 = 6, _s2 = 4;
+            int _e1 = 12, _e2 = 10;
+
+            foreach (var _string in _solution.solution(_my_string1, _s1, _e1))
+            {
+                Console.Write(_string + "");
+            }
+            Console.WriteLine();
+
+            foreach (var _string in _solution.solution(_my_string2, _s2, _e2))
+            {
+                Console.Write(_string + "");
+            }
+            Console.WriteLine();
         }
-
     }
     */
 }
