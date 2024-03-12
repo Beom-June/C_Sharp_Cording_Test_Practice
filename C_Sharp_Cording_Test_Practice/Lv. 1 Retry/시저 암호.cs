3e1d10eb9a9ce0a5b76eaa234fc6677f59a44af6
= <zxo9988@naver.com>
2024-03-12 11:24:43 +0900

    문제 추가
    3문제 추가

---
+++ C_Sharp_Cording_Test_Practice/Lv. 1 Retry/시저 암호.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Retry
+ * 어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 
+ * 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다. 
+ * 
+ * 예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다. 
+ * "z"는 1만큼 밀면 "a"가 됩니다. 
+ * 
+ * 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, 
+ * solution을 완성해 보세요.
+ */
+namespace C_Sharp_Cording_Test_Practice
+{
+    /*
+    public class Solution
+    {
+        public string solution(string s, int n)
+        {
+            string answer = "";
+            foreach (char _char in s)
+            {
+                if (_char != ' ')
+                {
+                    int _tmp = 0;
+
+                    //ASCII 코드
+                    //A=65, Z=90, a=97, z=122
+
+                    //대문자
+                    if ((int)_char < 91)
+                    {
+                        _tmp = (int)_char + n;
+                        if (_tmp > 90)
+                        {
+                            _tmp = 64 + (_tmp - 90);
+                        }
+                    }
+                    //소문자
+                    else
+                    {
+                        _tmp = (int)_char + n;
+                        if (_tmp > 122)
+                        {
+                            _tmp = 96 + (_tmp - 122);
+                        }
+                    }
+                    answer += Convert.ToChar(_tmp);
+                }
+                else
+                    answer += ' ';
+            }
+            return answer;
+        }
+    }
+    class 시저_암호
+    {
+        static void Main(string[] args)
+        {
+            Solution _solution = new Solution();
+
+            string _s01 = "AB";
+            string _s02 = "z";
+            string _s03 = "a B z";
+
+            int _n1 = 1, _n2 = 1, _n3 = 4;
+
+            Console.WriteLine(_solution.solution(_s01, _n1));       //  BC
+            Console.WriteLine(_solution.solution(_s02, _n2));       //  a
+            Console.WriteLine(_solution.solution(_s03, _n3));       //  e F d
+        }
+    }
+    */
+}
---
+++ C_Sharp_Cording_Test_Practice/최소직사각형.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 명함 지갑을 만드는 회사에서 지갑의 크기를 정하려고 합니다. 
+ * 다양한 모양과 크기의 명함들을 모두 수납할 수 있으면서, 작아서 들고 다니기 편한 지갑을 만들어야 합니다. 
+ * 이러한 요건을 만족하는 지갑을 만들기 위해 디자인팀은 모든 명함의 가로 길이와 세로 길이를 조사했습니다.
+ * 
+ * 아래 표는 4가지 명함의 가로 길이와 세로 길이를 나타냅니다.
+ * 
+ * 명함 번호	가로 길이	세로 길이
+ *     1	   60	       50
+ *     2	   30	       70
+ *     3	   60	       30
+ *     4	   80	       40
+ * 
+ * 가장 긴 가로 길이와 세로 길이가 각각 80, 70이기 때문에 80(가로) x 70(세로) 크기의 지갑을 만들면 모든 명함들을 수납할 수 있습니다. 
+ * 하지만 2번 명함을 가로로 눕혀 수납한다면 80(가로) x 50(세로) 크기의 지갑으로 모든 명함들을 수납할 수 있습니다. 
+ * 이때의 지갑 크기는 4000(=80 x 50)입니다.
+ * 
+ * 모든 명함의 가로 길이와 세로 길이를 나타내는 2차원 배열 sizes가 매개변수로 주어집니다. 
+ * 모든 명함을 수납할 수 있는 가장 작은 지갑을 만들 때, 지갑의 크기를 return 하도록 solution 함수를 완성해주세요.
+ */
+
+namespace C_Sharp_Cording_Test_Practice
+{
+    /*
+    public class Solution
+    {
+        public int solution(int[,] sizes)
+        {
+            int _maxWidth = 0;      //  가로
+            int _maxHeight = 0;     //  세로
+
+            for (int i = 0; i < sizes.GetLength(0); i++)
+            {
+                _maxWidth = Math.Max(_maxWidth, Math.Max(sizes[i, 0], sizes[i, 1]));
+                _maxHeight = Math.Max(_maxHeight, Math.Min(sizes[i, 0], sizes[i, 1]));
+            }
+            int answer = _maxWidth * _maxHeight;
+            return answer;
+        }
+    }
+    class 최소직사각형
+    {
+        static void Main(string[] args)
+        {
+            Solution _solution = new Solution();
+
+            int[,] _sizes01 = { { 60, 50 }, { 30, 70 }, { 60, 30 }, { 80, 40 } };
+            int[,] _sizes02 = { { 10, 7 }, { 12, 3 }, { 8, 15 }, { 14, 7 }, { 5, 15 } };
+            int[,] _sizes03 = { { 14, 4 }, { 19, 6 }, { 6, 16 }, { 18, 7 }, { 7, 11 } };
+
+            Console.WriteLine(_solution.solution(_sizes01));        //  4000
+            Console.WriteLine(_solution.solution(_sizes02));        //  120
+            Console.WriteLine(_solution.solution(_sizes03));        //  133
+        }
+    }
+    */
+}
