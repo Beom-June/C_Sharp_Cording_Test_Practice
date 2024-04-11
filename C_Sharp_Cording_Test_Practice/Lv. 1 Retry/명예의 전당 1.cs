f39506a4ae8115e61640b4da2ab3d584930cef62
= <zxo9988@naver.com>
2024-04-11 12:49:25 +0900

    문제 추가

---
+++ C_Sharp_Cording_Test_Practice/Lv. 1 Retry/명예의 전당 1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Retry
+ * "명예의 전당"이라는 TV 프로그램에서는 매일 1명의 가수가 노래를 부르고, 
+ * 시청자들의 문자 투표수로 가수에게 점수를 부여합니다. 매일 출연한 가수의 점수가 
+ * 지금까지 출연 가수들의 점수 중 상위 k번째 이내이면 해당 가수의 점수를 명예의 전당이라는 
+ * 목록에 올려 기념합니다. 
+ * 
+ * 즉 프로그램 시작 이후 초기에 k일까지는 모든 출연 가수의 점수가 명예의 전당에 오르게 됩니다. 
+ * k일 다음부터는 출연 가수의 점수가 기존의 명예의 전당 목록의 k번째 순위의 가수 점수보다 더 높으면, 
+ * 출연 가수의 점수가 명예의 전당에 오르게 되고 기존의 k번째 순위의 점수는 명예의 전당에서 내려오게 됩니다.
+ * 
+ * 이 프로그램에서는 매일 "명예의 전당"의 최하위 점수를 발표합니다. 
+ * 예를 들어, k = 3이고, 7일 동안 진행된 가수의 점수가 [10, 100, 20, 150, 1, 100, 200]이라면, 
+ * 명예의 전당에서 발표된 점수는 아래의 그림과 같이 [10, 10, 10, 20, 20, 100, 100]입니다.
+ */
+namespace C_Sharp_Cording_Test_Practice
+{
+    /*
+    public class Solution
+    {
+        public int[] solution(int k, int[] score)
+        {
+            int[] answer = new int[score.Length];
+            List<int> _board = new List<int>();
+
+            for (int i = 0; i < score.Length; i++)
+            {
+                if(_board.Count == k)
+                {
+                    if(_board[0] > score[i])
+                    {
+                        answer[i] = _board[0];
+                        continue;
+                    }
+                    _board.RemoveAt(0);
+                }
+                _board.Add(score[i]);
+                _board.Sort();
+                answer[i] = _board[0];
+            }
+            return answer;
+        }
+    }
+    class 명예의_전당_1
+    {
+        static void Main(string[] args)
+        {
+            Solution _solution = new Solution();
+
+            int[] _score01 = { 10, 100, 20, 150, 1, 100, 200 };
+            int[] _score02 = { 0, 300, 40, 300, 20, 70, 150, 50, 500, 1000 };
+
+            Console.WriteLine(String.Join(", ", _solution.solution(3, _score01)));      //  10, 10, 10, 20, 20, 100, 100
+            Console.WriteLine(String.Join(", ", _solution.solution(4, _score02)));      //  0, 0, 0, 0, 20, 40, 70, 70, 150, 300
+        }
+    }
+    */
+}
---
+++ C_Sharp_Cording_Test_Practice/Lv. 1 Retry/문자열 내 마음대로 정렬하기.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Retry
+ * 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 
+ * 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다. 
+ * 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면 
+ * 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
+ * 
+ * 제한 조건
+ * strings는 길이 1 이상, 50이하인 배열입니다.
+ * strings의 원소는 소문자 알파벳으로 이루어져 있습니다.
+ * strings의 원소는 길이 1 이상, 100이하인 문자열입니다.
+ * 모든 strings의 원소의 길이는 n보다 큽니다.
+ * 인덱스 1의 문자가 같은 문자열이 여럿 일 경우, 사전순으로 앞선 문자열이 앞쪽에 위치합니다.
+ */
+namespace C_Sharp_Cording_Test_Practice
+{
+    /*
+    public class Solution
+    {
+        public string[] solution(string[] strings, int n)
+        {
+            string[] answer = new string[strings.Length];
+
+            //  문자열 저장
+            for (int i = 0; i < strings.Length; i++)
+            {
+                //  문자열 배열인 `strings`에서 각 문자열을 선택한 후에
+                //  해당 문자열의 인덱스 `n`에 위치한 문자를 선택하는 것
+                answer[i] = (strings[i][n]) + strings[i];
+            }
+
+            //  정렬
+            Array.Sort(answer);
+
+            //  정렬된 문자열에 더했던 n번째 글자를 Substring(1)으로 제거
+            for (int j = 0; j < strings.Length; j++)
+            {
+                answer[j] = answer[j].Substring(1);
+            }
+            return answer;
+        }
+    }
+    class 문자열_내_마음대로_정렬하기
+    {
+        static void Main(string[] args)
+        {
+            Solution _solution = new Solution();
+
+            string[] _strings01 = { "sun", "bed", "car" };
+            string[] _strings02 = { "abce", "abcd", "cdx" };
+
+            Console.WriteLine(String.Join(", ", _solution.solution(_strings01, 1)));    //  "car", "bed", "sun"
+            Console.WriteLine(String.Join(", ", _solution.solution(_strings02, 2)));    //  "abcd", "abce", "cdx"
+        }
+    }
+    */
+}
