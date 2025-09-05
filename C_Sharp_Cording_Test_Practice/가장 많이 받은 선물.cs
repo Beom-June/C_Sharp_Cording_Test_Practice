3b92180f178796bec7341c875dc8ec8479524ef3
genjiboy0312 <zxo9988@naver.com>
2025-09-05 17:48:57 +0900

    Commit
    문제 추가

---
+++ C_Sharp_Cording_Test_Practice/가장 많이 받은 선물.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 선물을 직접 전하기 힘들 때 카카오톡 선물하기 기능을 이용해 축하 선물을 보낼 수 있습니다. 
+ * 당신의 친구들이 이번 달까지 선물을 주고받은 기록을 바탕으로 다음 달에 누가 선물을 많이 받을지 예측하려고 합니다.
+ * 
+ * 두 사람이 선물을 주고받은 기록이 있다면, 이번 달까지 두 사람 사이에 더 많은 선물을 준 사람이 다음 달에 선물을 하나 받습니다.
+ * 예를 들어 A가 B에게 선물을 5번 줬고, B가 A에게 선물을 3번 줬다면 다음 달엔 A가 B에게 선물을 하나 받습니다.
+ * 두 사람이 선물을 주고받은 기록이 하나도 없거나 주고받은 수가 같다면, 선물 지수가 더 큰 사람이 선물 지수가 더 작은 사람에게 선물을 하나 받습니다.
+ * 선물 지수는 이번 달까지 자신이 친구들에게 준 선물의 수에서 받은 선물의 수를 뺀 값입니다.
+ * 예를 들어 A가 친구들에게 준 선물이 3개고 받은 선물이 10개라면 A의 선물 지수는 -7입니다. B가 친구들에게 준 선물이 3개고 받은 선물이 2개라면 B의 선물 지수는 1입니다. 
+ * 만약 A와 B가 선물을 주고받은 적이 없거나 정확히 같은 수로 선물을 주고받았다면, 다음 달엔 B가 A에게 선물을 하나 받습니다.
+ * 만약 두 사람의 선물 지수도 같다면 다음 달에 선물을 주고받지 않습니다.
+ * 위에서 설명한 규칙대로 다음 달에 선물을 주고받을 때, 당신은 선물을 가장 많이 받을 친구가 받을 선물의 수를 알고 싶습니다.
+ * 
+ * 친구들의 이름을 담은 1차원 문자열 배열 friends 이번 달까지 친구들이 주고받은 선물 기록을 담은 1차원 문자열 배열 gifts가 매개변수로 주어집니다. 
+ * 이때, 다음달에 가장 많은 선물을 받는 친구가 받을 선물의 수를 return 하도록 solution 함수를 완성해 주세요.
+ */
+namespace C_Sharp_Cording_Test_Practice
+{
+    public class Solution
+    {
+        public int solution(string[] friends, string[] gifts)
+        {
+            int answer = 0;
+            Dictionary<string, int> _giftPoint = new Dictionary<string, int>();                                 // 친구별 선물 지수 (준 선물 개수 - 받은 선물 개수)
+            Dictionary<(string, string), int> _giftHistory = new Dictionary<(string, string), int>();           // 친구 간 선물 기록 (A가 B에게 준 선물 횟수)
+            Dictionary<string, int> _nextMonthGift = new Dictionary<string, int>();                             // 최종적으로 각 친구가 다음 달에 받을 선물 개수
+
+            foreach (string friend in friends)
+            {
+                _nextMonthGift.Add(friend, answer);
+            }
+
+            foreach (string gift in gifts)
+            {
+                // "A B" 형태의 문자열을 분리하여 A가 B에게 선물을 준 것으로 기록
+                var _parts = gift.Split(' ');
+                string _giver = _parts[0];
+                string _receiver = _parts[1];
+
+                // 선물 기록 갱신
+                var _key = (_giver, _receiver);
+                if (_giftHistory.ContainsKey(_key))
+                    _giftHistory[_key]++;
+                else
+                    _giftHistory[_key] = 1;
+            }
+
+            return answer;
+        }
+    }
+    internal class 가장_많이_받은_선물
+    {
+        static void Main(string[] args)
+        {
+            Solution _solution = new Solution();
+
+            string[] _friends01 = { "muzi", "ryan", "frodo", "neo" };
+            string[] _friends02 = { "joy", "brad", "alessandro", "conan", "david" };
+            string[] _friends03 = { "a", "b", "c" };
+
+            string[] _gifts01 = { "muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi" };
+            string[] _gifts02 = { "alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david" };
+            string[] _gifts03 = { "a b", "b a", "c a", "a c", "a c", "c a" };
+
+            Console.WriteLine(_solution.solution(_friends01, _gifts01));     //2
+            Console.WriteLine(_solution.solution(_friends02, _gifts02));     //4
+            Console.WriteLine(_solution.solution(_friends03, _gifts03));     //0
+        }
+    }
+}
